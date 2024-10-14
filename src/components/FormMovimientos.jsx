b4714b8e6132b7607374b312579603f45c5883f3
grossremolques <156919790+grossremolques@users.noreply.github.com>
2024-10-14 06:23:15 -0300

    Update FormMovimientos.jsx

--- src/components/FormMovimientos.jsx
+++ src/components/FormMovimientos.jsx
@@ -1,110 +1,92 @@
 import { useState, useEffect } from "react";
-import {
-  Col,
-  Row,
-  Form,
-  Button,
-  ButtonGroup,
-  ToggleButton,
-  Alert,
-  Modal,
-} from "react-bootstrap";
+import { Form, Button, Alert, Table, ButtonGroup } from "react-bootstrap";
 import { supabase } from "../API/client";
-import { useForm } from "react-hook-form";
-import GroupButtons from "./GroupButtons";
+import { useForm, useFieldArray } from "react-hook-form";
 import ModalComponent from "./ModalComponent";
 
+import {TypeRadioGroup} from "./TypeRadioGroup";
+import SelectComponentArea from "./SelectComponentArea";
+import TableSelectProducts from "./TableSelectProducts";
+
 function FormMovimientos() {
-  const [dataAreas, setDataAreas] = useState([]);
-  const [dataProductos, setDataProductos] = useState([]);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchAreas = async () => {
-      try {
-        const response = await supabase.from("areas").select("id, name");
-        if (response.error) {
-          throw response.error;
-        }
-        setDataAreas(response.data);
-      } catch (e) {
-        setError(e.message);
-      }
-    };
-    const fetchProductos = async () => {
-      try {
-        const response = await supabase.from("productos").select("id, name");
-        if (response.error) {
-          throw response.error;
-        }
-        //console.log(response.data);
-        setDataProductos(response.data);
-      } catch (e) {
-        setError(e.message);
-      }
-    };
-    fetchAreas();
-    fetchProductos();
-  }, []);
+  const [selectProducts, setSelectProducts] = useState([]);
+  const [deleteProducts, setDeleteProducts] = useState([]);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
-  //console.warn(errors);
-  const onSubmit = handleSubmit((data, errors) => {
-    console.log(data);
+    watch,
+    setValue,
+    control,
+  } = useForm({
+    defaultValues : {
+      products: [{name:'', stock:'',quantity: ''}]
+    }
   });
-  return (
-    <Form onSubmit={onSubmit} style={{ maxWidth: "500px", margin: "auto" }}>
-      {error && <Alert variant="danger">{error}</Alert>}
-      <GroupButtons/>
-      <Form.Group className="mt-3" controlId="id">
-        <Form.Label>Área</Form.Label>
-        <Form.Select
-          {...register("area", {
-            required: true,
-          })}
-        >
-          <option value="">Seleccione un área</option>
-          {dataAreas.map((area) => (
-            <option key={area.id + "-" + area.name} value={area.id}>
-              {area.name}
-            </option>
-          ))}
-        </Form.Select>
-      </Form.Group>
-      <ModalComponent data={dataProductos}/>
-      <Row className="g-1 mt-4">
-        <h5>Productos seleccionados</h5>
-        <Col>
-          <Form.Control
-            type="text"
-            placeholder="Nombre del producto"
-            readOnly
-            {...register('product_1', {
-                required: true
-            })}
-          />
-        </Col>
-        <Col className="col-2">
-          <Form.Control
-            type="number"
-            placeholder="Cant"
-            {...register('cant_product_1', {
-                required: true
-            })}
-          />
-        </Col>
-        <Col className="col-auto">
-        <Button variant="outline-danger" type="button"><i className="bi bi-trash3-fill"></i></Button>
-        </Col>
-        {/* <Col className="col-auto">
-        <Button variant="success" type="button" onClick={handleAddRow}><i className="bi bi-plus-lg"></i></Button>
-        </Col> */}
-      </Row>
+  const {fields, append, remove, replace} =useFieldArray({
+    control, name: "products"
+  })
 
-      <Button className="mt-4" variant="primary" type="submit">
-        Submit
+  const onError = (errors, e) => console.log("No enviado", errors, e);
+  const onSubmit = (data, e) => console.log("Enviado", data, e);
+
+  const handleSelectProducts = (data) => {
+    console.log(data)
+    setSelectProducts(data);
+  };
+
+  const handleDeleteRow = (e) => {
+    const row = e.target.closest("button").parentNode.parentNode;
+    const idProduct = row.firstChild.title;
+    setDeleteProducts([...deleteProducts, idProduct]);
+    setSelectProducts([...selectProducts].filter(elem => elem.id != idProduct))
+  };
+  return (
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      style={{ maxWidth: "500px", margin: "auto" }}
+    >
+      <TypeRadioGroup register={register} errors={errors} />
+      <SelectComponentArea
+        register={register}
+        errors={errors}
+        setValue={setValue}
+      />
+      <ModalComponent
+        onSelectProducts={handleSelectProducts}
+        deleteProducts={deleteProducts}
+        register={register}
+        handleSubmit={handleSubmit}
+        watch={watch}
+      />
+      {errors.button && (
+        <div className="form-text text-danger">{errors.button.message}</div>
+      )}
+      <h5 className="mt-4">Productos seleccionados</h5>
+      <TableSelectProducts
+        data={selectProducts}
+        register={register}
+        errors={errors}
+        handleDeleteRow={handleDeleteRow}
+        watch={watch}
+        fields={fields}
+        remove={remove}
+        append={append}
+        replace={replace}
+      />
+      <Button
+        className="mt-4"
+        variant="primary"
+        type="submit"
+        {...register("button", {
+          validate: () => {
+            console.log(watch('products'))
+            return selectProducts.length > 0 || "No ha seleccionado productos";
+          },
+        })}
+      >
+        Guardar
       </Button>
     </Form>
   );
