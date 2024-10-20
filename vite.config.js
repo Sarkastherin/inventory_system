82bb0011dd551f058bc388458e6e6d7877e18b17
sarkastherin <kathejudith@gmail.com>
2024-10-20 12:17:01 -0300

    try to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://Sarkastherin.github.io/inventory_system/'
+  define: {
+    'process.env': {}
+  },
+  base: 'inventory_system'
 })
 //https://<USERNAME>.github.io/<REPO>/
 //https://github.com/Sarkastherin/inventory_system
