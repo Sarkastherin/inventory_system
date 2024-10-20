c4d566234e8d352bef6a9b5a991fa70e72444b84
sarkastherin <kathejudith@gmail.com>
2024-10-20 14:18:52 -0300

    cambiando direcciones

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  define: {
-    'process.env': {}
-  },
+  base: isProduction ? '/inventory_system/' : '/',
   define: {
     'process.env.VITE_PROYECT_URL': JSON.stringify(process.env.VITE_PROYECT_URL),
     'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
