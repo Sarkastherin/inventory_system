960d1856c779b0bbc0a4b18d5320762197bb2c98
sarkastherin <kathejudith@gmail.com>
2024-10-20 12:59:07 -0300

    my first commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/inventory_system/',
   define: {
     'process.env': {}
-  },
-  base: '/inventory_system/'
+  }  
 })
 //https://<USERNAME>.github.io/<REPO>/
 //https://github.com/Sarkastherin/inventory_system
