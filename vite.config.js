bd6b8e974c439ad49f6446e2c74b5996d90c6951
sarkastherin <kathejudith@gmail.com>
2024-10-20 11:54:20 -0300

    fixed ur base on vite.config for the second time

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://github.com/Sarkastherin/inventory_system'
+  base:'https://Sarkastherin.github.io/inventory_system/'
 })
+//https://<USERNAME>.github.io/<REPO>/
+//https://github.com/Sarkastherin/inventory_system
